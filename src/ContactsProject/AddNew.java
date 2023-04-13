f70e613e8b115a87c8bbb10b6bf0f152c768d401
Austin Joiner <austin.j.joiner1@gmail.com>
2023-04-13 10:48:52 -0400

    finished contact

--- src/ContactsProject/AddNew.java
+++ src/ContactsProject/AddNew.java
@@ -6,17 +6,14 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 
-
 public class AddNew{
     private static final String contactFilePath = "data/contacts.txt";
 
     public static void addContact(String name, String number) throws IOException {
-
 
         ContactsFileCreation.getContactNameList().add(name);
         ContactsFileCreation.getContactNumberList().add(number);
         //System.lineSeparator() - separates lines like /n but ensures it works for all systems
         Files.write(Paths.get(contactFilePath), (name + System.lineSeparator() + number + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
     }
-
 }
